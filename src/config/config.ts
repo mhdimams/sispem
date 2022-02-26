e2d9e684c3be10aa38147c982ac9d329d4202458
Muhammad Furqan Soedono <furqonsoedono71@gmail.com>
2022-02-26 15:18:20 +0700

    change base url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8000/apiweb/v1';
+export const BASE_URL = 'https://sispem.herokuapp.com/apiweb/v1';
 export const BASE_URL_LOGIN = BASE_URL + '/auth/request-token';
 export const BASIC_KEY = '';
